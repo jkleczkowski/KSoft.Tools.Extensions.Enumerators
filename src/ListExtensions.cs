822ec1112c4ce83772bd9e9d2d4007371b62244a
Jacek Kleczkowski <jkleczkowski@gmail.com>
2018-02-25 14:08:10 +0100

    AddIfNotExists extensions

---
+++ src/ListExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace KSoft.Tools.Extensions
+{
+  public static class ListExtensions
+  {
+    public static void AddIfNotExists<T>(this IList<T> list, T el)
+    {
+      if (!list.Contains(el))
+      {
+        list.Add(el);
+      }
+    }
+    
+    public static void AddRangeIfNotExists<T>(this IList<T> list, IEnumerable<T> el)
+    {
+      foreach (var name in el) list.AddIfNotExists(name);
+    }
+  }
+}
---
+++ test/ListAddIfNotExistsTests.cs
@@ -0,0 +1,45 @@
+using System.Collections.Generic;
+using System.Linq;
+using KSoft.Tools.xUnit.Logger;
+using Xunit;
+using Xunit.Abstractions;
+
+
+namespace KSoft.Tools.Extensions.Lists.Tests
+{
+  public class ListAddIfNotExistsTests
+  {
+    protected ITestOutputHelper output;
+    private TestLogger logger;
+
+    public ListAddIfNotExistsTests(ITestOutputHelper output)
+    {
+      this.output = output;
+      logger = new TestLogger(output);
+    }
+
+    [Fact]
+    public void test_add_one_element_to_IList()
+    {
+      IList<string> list = new List<string>()
+      {
+        "Element A"
+      };
+      list.AddIfNotExists("Element B");
+      list.Count.ShouldBe(2);
+      logger.Info("success");
+    }
+    [Fact]
+    public void test_multiple_elements_to_IList()
+    {
+      IList<string> list = new List<string>()
+      {
+        "Element A"
+      };
+      list.AddRangeIfNotExists(new []{ "Element B" , "Element C" });
+      list.Count.ShouldBe(3);
+      logger.Info("success");
+    }
+
+  }
+}
